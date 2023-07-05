4ee9b9ebcaaff15b5d70a6962d86cc6055984a84
MinhDuyDEV <94704296+MinhDuyDEV@users.noreply.github.com>
2023-07-05 20:45:12 +0700

    modal component (admin)

--- ecommerce-admin/app/(root)/page.tsx
+++ ecommerce-admin/app/(root)/page.tsx
@@ -1,12 +1,19 @@
-import { UserButton } from "@clerk/nextjs";
-import React from "react";
+"use client";
+
+import React, { useEffect } from "react";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
 
 const SetupPage = () => {
-  return (
-    <div>
-      <UserButton afterSignOutUrl="/"></UserButton>
-    </div>
-  );
-};
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
 
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
+  return <div className="p-4">Root page</div>;
+};
 export default SetupPage;
